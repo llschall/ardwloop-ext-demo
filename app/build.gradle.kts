154688dfbae6dfc7b299689a683ea9b1955ec4ef
llschall <llschall@gmx.com>
2024-09-17 23:25:41 +0200

    [Build] Add ardwloop-ext 0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ android {
 
 dependencies {
 
+    implementation(libs.ardwloop.ext)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
