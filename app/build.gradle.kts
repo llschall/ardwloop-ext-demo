a11dc1e658f1a4b635ffcfe661c9b6005a3be75d
llschall <llschall@gmx.com>
2024-12-30 00:03:26 +0100

    [Build] Bump tp 0.9.16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ardwloop.ext.demo"
         minSdk = 24
         targetSdk = 35
-        versionCode = 15
-        versionName = "0.9.15"
+        versionCode = 16
+        versionName = "0.9.16"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
