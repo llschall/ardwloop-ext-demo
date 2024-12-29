7dc78d1d533fff2f3932889e5c5a66fa3db0b54f
llschall <llschall@gmx.com>
2024-12-29 12:29:32 +0100

    [Build] Bump ardwloop-ext to 0.1.9

--- app/src/test/java/ardwloop/ext/demo/BuildTest.java
+++ app/src/test/java/ardwloop/ext/demo/BuildTest.java
@@ -18,9 +18,9 @@ public class BuildTest {
 
     @Test
     public void checkArdwloop() {
-        assertEquals("0.2.8", ArdwloopStarter.VERSION);
+        assertEquals("0.2.9", ArdwloopStarter.VERSION);
         assertEquals(1001, ArdwloopStarter.VERSION_INT);
-        assertEquals("0.1.8", new ArdwloopExtStarter().getVERSION());
+        assertEquals("0.1.9", new ArdwloopExtStarter().getVERSION());
         assertEquals(1001, new ArdwloopExtStarter().getVERSION_INT());
     }
 }
